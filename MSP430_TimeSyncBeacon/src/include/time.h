2d3fc6d5391e8fd21a81701ada0dadd671ea5103
triszalo <triszalo@freemail.hu>
2013-05-21 12:31:20 +0200

    little modifications;  - time modul deleted  - Grace_init() added

--- MSP430_TimeSyncBeacon/src/include/time.h
+++
@@ -1,6 +0,0 @@
-#ifndef TIME_H_
-#define TIME_H_
-
-void time_init(void);
-
-#endif //TIME_H_
--- MSP430_TimeSyncBeacon/src/main.c
+++ MSP430_TimeSyncBeacon/src/main.c
@@ -1,11 +1,19 @@
 
+
+#include <msp430.h>
+#include <ti/mcu/msp430/Grace.h>
+
 #include "tlc5916.h"
-#include "time.h"
+#include "gray.h"
 
 int main(void){
 
+	Grace_init();	// Activate Grace-generated configuration
+
+	/* Start Timer_A */
+	TA0CTL |= MC_1;	/* Start timer in up mode */
+
 	tlc5916_init();
-	time_init();
 
 	#if 1
 	tlc5916_write_led(0, 1);
@@ -15,8 +23,24 @@ int main(void){
 	tlc5916_latch();
 	#endif
 	
-	while(1);
+	while(1){
 
-	return 0;
+	}
+
+}
+
+void calc(uint32_t time_ms){
+
+	const int threshold = 32;
+	uint32_t low = time_ms % threshold;
+	uint32_t high = time_ms / threshold;
+
+	uint32_t run = low;
+	uint32_t gray = binaryToGray(high);
+
+	tlc5916_write_led(0 + run, 1);
+	tlc5916_write_leds(&gray, 32, 32);
+
+	tlc5916_send();
 
 }

5183990ad7bdd259c05315451e1383abc6f8bfbb
Kristof <kristof@aut.bme.hu>
2013-07-05 17:48:26 +0200

    MarkovFsmTest -= higher functions moved to libImageFilter.TransitionStat

--- MarkovFsmTest/src/main.cpp
+++ MarkovFsmTest/src/main.cpp
@@ -20,105 +20,6 @@ using namespace smeyel;
 using namespace LogConfigTime;
 
 
-unsigned char lastValue = -1;
-bool lastIsTargetArea = false;
-int runLength = 0;
-void addRunLengthQuantizedValue(TransitionStat *stat, unsigned char value, bool isTargetArea)
-{
-	if (value==lastValue)
-	{
-		runLength++;
-		lastIsTargetArea = isTargetArea;
-	}
-	else
-	{
-		int quantizedRunLength = 0;
-		if (runLength>=5)	quantizedRunLength = 1;
-		if (runLength>=10)	quantizedRunLength = 2;
-		if (runLength>=20)	quantizedRunLength = 3;
-		for (int i=0; i<quantizedRunLength; i++)
-			stat->addValue(lastValue,lastIsTargetArea);
-		lastValue = value;
-		runLength = 1;
-		lastIsTargetArea = isTargetArea;
-	}
-}
-
-unsigned char lastScore = 0;
-unsigned char getScoreForRunLengthQuantizedValue(TransitionStat *stat, unsigned char value)
-{
-	if (value==lastValue)
-	{
-		runLength++;
-	}
-	else
-	{
-		int quantizedRunLength = 0;
-		if (runLength>=5)	quantizedRunLength = 1;
-		if (runLength>=10)	quantizedRunLength = 2;
-		if (runLength>=20)	quantizedRunLength = 3;
-		for (int i=0; i<quantizedRunLength-1; i++)	// Fetch n-1 values...
-			stat->getScoreForValue(value);
-		unsigned char score = 0;	// Default for initial cases...
-		if (quantizedRunLength-1>0)
-		{
-			score = stat->getScoreForValue(value);
-		}
-		lastScore = score;
-		lastValue = value;
-		runLength = 1;
-	}
-	return lastScore;
-}
-
-void feedImageIntoTransitionStat(TransitionStat *stat, Mat &image, bool isOn)
-{
-	// Assert for only 8UC1 output images
-	OPENCV_ASSERT(image.type() == CV_8UC1,"feedImageIntoTransitionStat","Image type is not CV_8UC1");
-
-	// Go along every pixel and do the following:
-	for (int row=0; row<image.rows; row++)
-	{
-		// Calculate pointer to the beginning of the current row
-		const uchar *ptr = (const uchar *)(image.data + row*image.step);
-
-		// Go along every BGR colorspace pixel
-		for (int col=0; col<image.cols; col++)
-		{
-			unsigned char value = *ptr++;
-
-			//stat->addValue(value,isOn);
-			addRunLengthQuantizedValue(stat,value,isOn);
-		}	// end for col
-	}	// end for row
-}
-
-void getScoreMaskForImage(TransitionStat *stat, Mat &src, Mat &dst)
-{
-	// Assert for only 8UC1 output images
-	OPENCV_ASSERT(src.type() == CV_8UC1,"getScoreMaskForImage","src type is not CV_8UC1");
-	OPENCV_ASSERT(dst.type() == CV_8UC1,"getScoreMaskForImage","dst type is not CV_8UC1");
-	OPENCV_ASSERT(src.rows=dst.rows,"getScoreMaskForImage","src and dst size does not match");
-	OPENCV_ASSERT(src.cols=dst.cols,"getScoreMaskForImage","src and dst size does not match");
-
-	// Go along every pixel and do the following:
-	for (int row=0; row<src.rows; row++)
-	{
-		// Calculate pointer to the beginning of the current row
-		const uchar *srcPtr = (const uchar *)(src.data + row*src.step);
-		uchar *dstPtr = (uchar *)(dst.data + row*dst.step);
-
-		// Go along every BGR colorspace pixel
-		for (int col=0; col<src.cols; col++)
-		{
-			unsigned char value = *srcPtr++;
-
-			//*dstPtr = stat->getScoreForValue(value);
-			*dstPtr = getScoreForRunLengthQuantizedValue(stat,value);
-			dstPtr++;
-		}	// end for col
-	}	// end for row
-}
 
 void test_frameSaver()
 {
@@ -176,13 +77,9 @@ void processImages(istream *filenameListFile, bool isOn, LutColorFilter *filter,
 		filter->Filter(&src,&lut,NULL);
 		filter->InverseLut(lut,visLut);
 
-		feedImageIntoTransitionStat(stat,lut,isOn);
-
+		stat->addImage(lut,isOn);
 	}
 }
-
-// Type of callback for promising nodes
-typedef void (*notifycallbackPtr)(SequenceCounterTreeNode *node, float precision);
 
 bool callbackVerbose=true;
 void callback(SequenceCounterTreeNode *node, float precision)
@@ -201,7 +98,8 @@ void callback(SequenceCounterTreeNode *node, float precision)
 	SequenceCounterTreeNode *parent = node->getParentNode();
 	while(parent)
 	{
-		cout << parent->getInputValueForChild(current) << " ";
+		if(callbackVerbose)
+			cout << parent->getInputValueForChild(current) << " ";
 		current = parent;
 		parent = current->getParentNode();
 	}
@@ -211,45 +109,6 @@ void callback(SequenceCounterTreeNode *node, float precision)
 		cout << "Subtree:" << endl;
 		node->showCompactRecursive(0,1);
 	}
-}
-
-
-void checkNode(SequenceCounterTreeNode *node, float sumOn, float sumOff, int maxInputValue, notifycallbackPtr callback)
-{
-	int onNum = node->getCounter(COUNTERIDX_ON);
-	int offNum = node->getCounter(COUNTERIDX_OFF);
-	int sumNum = onNum+offNum;
-	float onRate = onNum / sumOn;
-	float offRate = offNum / sumOff;
-	float precision = onRate / (onRate+offRate);
-	if (precision > 0.8 && sumNum>10 )
-	//if (precision > 0.95 && sumNum>5 )
-	//if (precision > 0.95 && sumNum>200 )
-	{
-		(*callback)(node, precision);
-	}
-	// Continue on children
-	for (int i=0; i<=maxInputValue; i++)
-	{
-		SequenceCounterTreeNode *child = node->getChildNode(i,false);
-		if (child)
-		{
-			checkNode(child,sumOn,sumOff,maxInputValue,callback);
-		}
-	}
-}
-
-void findClassifierSequences(TransitionStat *stat)
-{
-	// Recursively go along every sequence and compare on/off frequencies
-	SequenceCounterTreeNode *root = stat->counterTreeRoot;
-
-	SequenceCounterTreeNode *node = root;
-
-	float sumOn = (float)root->getCounter(COUNTERIDX_ON);
-	float sumOff = (float)root->getCounter(COUNTERIDX_OFF);
-
-	checkNode(root,sumOn,sumOff,7,callback);
 }
 
 void test_mkStatFromImageList(const char *offImageFilenameList, const char *onImageFilenameList)
@@ -280,7 +139,7 @@ void test_mkStatFromImageList(const char *offImageFilenameList, const char *onIm
 
 	stat->counterTreeRoot->showCompactRecursive(0,1);
 
-	findClassifierSequences(stat);
+	stat->findClassifierSequences(callback);
 
 	// -------------- Now start camera and apply statistics (auxScore mask) to the frames
 	CameraLocalProxy *camProxy0 = new CameraLocalProxy(VIDEOINPUTTYPE_PS3EYE,0);
@@ -304,7 +163,7 @@ void test_mkStatFromImageList(const char *offImageFilenameList, const char *onIm
 		lutColorFilter->Filter(&src,&lut,NULL);
 		lutColorFilter->InverseLut(lut,visLut);
 
-		getScoreMaskForImage(stat,lut,score);
+		stat->getScoreMaskForImage(lut,score);
 
 		imshow("SRC",src);
 		imshow("LUT",visLut);
@@ -335,7 +194,7 @@ void test_singleFeed()
 	imshow("TestImage",src);
 	imshow("LUT",lutVis);
 	waitKey(0);
-	feedImageIntoTransitionStat(stat, lut, true);
+	stat->addImage(lut, true);
 
 	src.setTo(Scalar(0,0,0));
 	rectangle(src,Point2d(25,0),Point2d(30,49),Scalar(0,0,255));
@@ -344,13 +203,13 @@ void test_singleFeed()
 	imshow("TestImage",src);
 	imshow("LUT",lutVis);
 	waitKey(0);
-	feedImageIntoTransitionStat(stat, lut, false);
+	stat->addImage(lut, false);
 
 	stat->counterTreeRoot->showCompactRecursive(0,1);
 	stat->counterTreeRoot->getAndStoreSubtreeSumCounter(COUNTERIDX_ON);
 	stat->counterTreeRoot->getAndStoreSubtreeSumCounter(COUNTERIDX_OFF);
 
-	findClassifierSequences(stat);
+	stat->findClassifierSequences(callback);
 
 	cout << "done" << endl;
 }
@@ -390,7 +249,7 @@ void test_mkStatInteractive()
 		lutColorFilter->Filter(&src,&lut,NULL);
 		lutColorFilter->InverseLut(lut,visLut);
 
-		getScoreMaskForImage(stat,lut,score);
+		stat->getScoreMaskForImage(lut,score);
 
 		imshow("SRC",src);
 		imshow("LUT",visLut);
@@ -404,11 +263,11 @@ void test_mkStatInteractive()
 			cout << "Exiting" << endl;
 			break;
 		case '+':	// on
-			feedImageIntoTransitionStat(stat,lut,true);
+			stat->addImage(lut,true);
 			cout << "Saved ON" << endl;
 			break;
 		case '-':	// off
-			feedImageIntoTransitionStat(stat,lut,false);
+			stat->addImage(lut,false);
 			cout << "Saved OFF" << endl;
 			break;
 		case 'l':	// learn
@@ -416,7 +275,7 @@ void test_mkStatInteractive()
 			stat->counterTreeRoot->getAndStoreSubtreeSumCounter(COUNTERIDX_ON);
 			if (markovChainOrder<4)
 				stat->counterTreeRoot->showCompactRecursive(0,1);
-			findClassifierSequences(stat);
+			stat->findClassifierSequences(callback);
 			cout << "Trained" << endl;
 			break;
 		case 'r':	// reset

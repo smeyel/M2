0025dff3c38fbca022ac95d92e2d0e81d40b46ba
Kristof <kristof@aut.bme.hu>
2013-08-01 14:10:59 +0200

    Upgrade ImageProcessing (new bounding box collection system), removed M10 related projects.

--- MarkovFsmTest/include/fsmlearning.h
+++
@@ -1,7 +0,0 @@
-#ifndef __FSMLEARNING_H
-#define __FSMLEARNING_H
-
-void test_graphOpt();
-void test_mkStatFromImageList(const char *offImageFilenameList, const char *onImageFilenameList);
-
-#endif
